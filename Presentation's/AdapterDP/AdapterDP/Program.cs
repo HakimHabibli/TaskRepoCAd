7dc0271c3cf6ae9fbb2f8ad6734eff2fd057730a
HakimHabibli <HakimHabibli>
2024-07-14 21:41:24 +0400

    New Code Added or updated
    some changes to the code base : -7/-02-21-41-24

---
+++ Presentation's/AdapterDP/AdapterDP/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AdapterDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Target target = new Adapter();
+            target.Request();
+        }
+        public class Target
+        {
+            public virtual void Request()
+            {
+                Console.WriteLine("Targer Request Method");
+            }
+        }
+        public class Adapter : Target
+        {
+            private Adaptee adaptee = new Adaptee();
+            public override void Request()
+            {
+                adaptee.SpecialRequest();
+            }
+        }
+        public class Adaptee
+        {
+            public void SpecialRequest()
+            {
+                Console.WriteLine("Adaptee Request Method");
+            }
+        }
+    }
+}
